617d253250674cc97adc8c56c8aa8039c4a569a8
Philly Cai <philly.cai@storehub.com>
2021-12-10 14:03:54 +0800

    chore: update script

--- workplace-PR-extension.js
+++ workplace-PR-extension.js
@@ -1,62 +1,72 @@
 // ==UserScript==
 // @name         PR Helper for Workplace
-// @namespace    https://www.storehub.com/
+// @namespace    https://github.com/Hephaest/helper-scripts
 // @version      0.1
 // @description  Pre-filled PR info in Workplace
 // @author       Hephaest
 // @icon         https://avatars.githubusercontent.com/u/37981444?v=4
 // @match        https://www.workplace.com/
-// @updateURL    https://github.com/Hephaest/helper-scrips/blob/main/workplace-PR-extension.js
+// @updateURL    https://github.com/Hephaest/helper-scripts/blob/main/workplace-PR-extension.js
 // @grant        none
 // @license      MIT
 // @run-at       document-idle
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
+  var whiteList = [];
   var messageDivElement = null;
-  var timer = setInterval(setUpMessageListener, 5000);
   var dataTransfer = new DataTransfer();
 
-   function dispatchPaste(target, text) {
-     // this may be 'text/html' if it's required
-     dataTransfer.setData('text/plain', text);
-
-     target.dispatchEvent(
-       new ClipboardEvent('paste', {
-         clipboardData: dataTransfer,
-         // need these for the event to reach Draft paste handler
-         bubbles: true,
-         cancelable: true
-       })
-     );
-
-     // clear DataTransfer Data
-     dataTransfer.clearData();
-   }
-
-  function setUpMessageListener() {
+  var timer = setInterval(function () {
     messageDivElement = document.querySelector('div[contenteditable]');
-    if (!messageDivElement) return;
-    messageDivElement.addEventListener('keyup', function(event) {
-      var input = event.target.innerText;
-      if (!hasPRCmdTriggered(input)) return;
-      setUpPRTemplate(event);
-    });
-    clearInterval(timer);
-  };
+    if (!messageDivElement || messageDivElement.onkeyup) return;
+    messageDivElement.onkeyup = setUpMessageListener;
+  }, 3000);
+
+  function setUpMessageListener(event) {
+    var input = event.target.innerText;
+    if (!isInWhiteList() || !hasPRCmdTriggered(input)) return;
+    setUpPRTemplate(event);
+    //clearInterval(timer);
+  }
+
+  function isInWhiteList() {
+    var pathname = window.location.pathname;
+    var regex = /\/chat\/t\//gm;
+    var chatId = pathname.replace(regex, '');
+    var result = whiteList.includes(chatId);
+    return result;
+  }
 
   function hasPRCmdTriggered(input) {
     return input === 'PRT' || input === 'prt';
   }
 
   function setUpPRTemplate(event) {
-    var templateText = ":\n" +
-                       "PR:\n" +
-                       "ChangeLog:\n" +
-                       "Reviewers:\n" +
-                       "辛苦各位有空的时候帮忙看一下🙏";
+    var templateText =
+      ':\n' +
+      'PR:\n' +
+      'ChangeLog:\n' +
+      'Reviewers:\n' +
+      '辛苦各位有空的时候帮忙看一下🙏';
     dispatchPaste(event.target, templateText);
+  }
+
+  function dispatchPaste(target, text) {
+    dataTransfer.setData('text/plain', text);
+
+    target.dispatchEvent(
+      new ClipboardEvent('paste', {
+        clipboardData: dataTransfer,
+        // need these for the event to reach Draft paste handler
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    // clear DataTransfer Data
+    dataTransfer.clearData();
   }
 })();
